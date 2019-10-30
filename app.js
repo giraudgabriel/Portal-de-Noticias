b9d780de5361f30074b0b3903e418dfd2bbe4ae6
gabrielmulticontas <gabriel@multicontas.com>
2019-10-30 16:46:18 -0300

    #CHANGE: Usando consign para refatora as rotas

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const app = require('./config/server')
 
-require('./app/routes/noticias')(app)
-require('./app/routes/home')(app)
-require('./app/routes/formulario')(app)
+// require('./app/routes/noticias')(app)
+// require('./app/routes/home')(app)
+// require('./app/routes/formulario')(app)
 
 
 app.listen(3000, () => {
