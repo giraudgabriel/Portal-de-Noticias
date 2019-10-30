57592679fe856c30053b78f5d85f4f3c713c6eeb
gabrielmulticontas <gabriel@multicontas.com>
2019-10-30 16:12:50 -0300

    #FEATURE: Busca de noticias do mysql & node & ejs

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,20 @@
 module.exports = (app) => {
     app.get('/noticias', (req, res) => {
-        res.render("noticias/noticias")
+        var mysql = require('mysql')
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+        })
+
+        connection.query("SELECT * FROM NOTICIAS", (error, result) => {
+            res.render("noticias/noticias", {
+                noticias: result
+            })
+        })
+
+        
     })
 }
