5b27d04a80faf7c4942db8ab52cb56a485d78fae
giraudgabriel <gabriel.giraud@fatec.sp.gov.br>
2019-11-03 03:20:55 -0300

    #FEATURE: express-validator in post

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,21 @@ module.exports = (app) => {
     })
 
     app.post('/noticias/salvar', (req, res) => {
+
+        req.assert('titulo', 'Título é obrigatório').notEmpty()
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty()
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100)
+        req.assert('autor', 'Autor é obrigatório').notEmpty()
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty()
+        req.assert('noticia','Notícia é obrigatória').notEmpty()
+
+        let erros = req.validationErrors()
+
+        if(erros){
+            res.render("admin/form_add_noticia")
+            return
+        }
+
         let noticiasModel = new app.app.models.NoticiasDAO(app.config.dbConnection())
         noticiasModel.salvarNoticia(req.body, () => res.redirect('/noticias'))
     })
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -9,6 +10,8 @@ app.set('views', './app/views')
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+app.use(expressValidator())
 
 consign()
     .include('app/routes')
